81436c0b4537a7ae719b69328105f75f90c7c528
Valent142003 <valent142003@srru.ac.th>
2023-04-05 01:49:15 +0700

    README

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="box-data w-100 border m-auto d-flex justify-content-center align-items-center mt-5 row">
-        <img src="https://cdn.discordapp.com/attachments/1092068663827370044/1092860339672076308/kEhrQay_2.png" class="rounded mx-auto w-100 img-profile my-3 shadow p-0" alt="...">
+        <img src="https://cdn.discordapp.com/attachments/1092068663827370044/1092860339672076308/kEhrQay_2.png" class="rounded mx-auto img-thumbnail my-3 shadow " alt="...">
         <p class="h1 text-center">VLTs</p>
         <div></div>
     </div>
