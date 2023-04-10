533b73f15ef4f6596adab4b990bc595f3b2513e1
Valent142003 <valent142003@srru.ac.th>
2023-04-11 02:42:20 +0700

    edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,37 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
 
-@section('content')
-<div class="box-data w-100 border m-auto my-5 d-flex justify-content-center align-items-center row">
-    <figure class="image img-profile my-5">
-        <img class="is-rounded" src="https://cdn.discordapp.com/attachments/861146699857395742/1094326034284941372/image.png">
-    </figure>
-    <p class="h1 text-center">VLTs</p>
-    <div></div>
-</div>
-@endsection
+<head>
+    <meta charset="utf-8">
+    <!-- meta responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- icon bar -->
+    <link rel="icon" href="https://cdn.discordapp.com/attachments/1092068663827370044/1092860339672076308/kEhrQay_2.png" type="image/icon type">
+    <!-- Title -->
+    <title>VLTs</title>
+    <!-- Boostrap css link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <!-- Custom css link -->
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+    <section class="fullscreen d-flex align-items-center justify-content-center ">
+        <div class="box-content d-flex align-items-center justify-content-center  border rounded-3 w-100 h-100 bg-dark text-light row p-5">
+            <div>
+            <img src="https://i.pinimg.com/564x/a2/35/2e/a2352e276e656d895d18b0601d8935e6.jpg" class="image is-128x128 my-5 mx-auto rounded-3" alt="">
+            <p class="text-center h3">VLTs</p>
+            <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis quisquam, id voluptatibus sint quos beatae cupiditate blanditiis nihil incidunt culpa aspernatur nostrum minima facilis consequuntur explicabo vel totam placeat! Iure.</p>
+        </div>
+        </div>
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
